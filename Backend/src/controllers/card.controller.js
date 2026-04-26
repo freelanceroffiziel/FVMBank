0b703656a5969c9d9fc552c1f5dd6607a3901983
Smitcheal <smitcheal431@gmail.com>
2026-04-27 00:04:27 +0100

    updated backend controller

--- Backend/src/controllers/card.controller.js
+++ Backend/src/controllers/card.controller.js
@@ -107,7 +107,7 @@ exports.requestCard = async (req, res) => {
 };
 
 
-// helpers
+// helperssss
 function generateCardNumber() {
   return "**** **** **** " + Math.floor(1000 + Math.random() * 9000);
 }
